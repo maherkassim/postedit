5a79f70b8a0daf467bce66683ab26af3c9d8dacf
Maher Kassim <kassim.maher@gmail.com>
2013-09-24 07:44:51 -0400

    Continued implementation of UpdatePost: 	Modified NewPost to accomodate changes to post creation/update 	Added methods for creating languages and key dictionaryitems if missing 	Tested & corrected updating text blocks 	Started on image input

--- post_generator/views/editpost.py
+++ post_generator/views/editpost.py
@@ -22,6 +22,12 @@ from post_generator.models.dictionaryitem import DictionaryItem
 PRINT_ID = 1
 PRINT_IMG_ID = 2
 
+def create_lang_objects():
+    languages = []
+    for lang in ['english', 'somali', 'french', 'arabic']:
+        languages.append(Language.objects.create(full_name=lang))
+    return languages
+
 def create_print_objs():
     print_defaults = {
         'english':"Print Recipe",
@@ -70,10 +76,10 @@ def NewPost(request):
     # Create new Post object initialized with placeholder title
     temp_title = "New Post " + timezone.now().strftime("%s %s" % ("%Y-%m-%d",
                                                                   "%H:%M:%S"))
-    title_obj = DictionaryItem.objects.create(english = temp_title)
-    post_obj = Post(
-        link="http://xawaash.com/?p=",
+    title_obj = DictionaryItem.objects.create(english=temp_title)
+    post_obj = Post.objects.create(
         title=title_obj,
+        link="http://xawaash.com/?p=",
         pub_date=timezone.now()
     )
     post_obj.save()
@@ -100,7 +106,7 @@ def EditPost(request, post_id):
 def sub_dict(dictionary, regex):
     # Gives sub-dictionary where all keys contain string
     return dict((key, value)
-        for key,value in dictionary.iteritems() if regex.search(key))
+        for key,value in dictionary.iteritems() if regex.match(key))
 
 def update_dictionaryitem(request, lang_obj_list, field_name, item_id=0):
     query_args = Q()
@@ -116,7 +122,7 @@ def update_dictionaryitem(request, lang_obj_list, field_name, item_id=0):
         else:
             item_obj = DictionaryItem.objects.filter(query_args)[:1].get()
     except DictionaryItem.DoesNotExist:
-        item_obj = DictionaryItem(**update_args)
+        item_obj = DictionaryItem.objects.create(**update_args)
     else:
         item_obj = DictionaryItem(id=item_obj.id, **update_args)
     
@@ -125,14 +131,12 @@ def update_dictionaryitem(request, lang_obj_list, field_name, item_id=0):
 
 def update_post_title(request, post_obj, lang_obj_list):
     # Update Post object with new link and title
-    
-    title_obj = update_dictionaryitem(request, lang_obj_list, '-title')
+    title_obj = update_dictionaryitem(request, lang_obj_list, '-title', post_obj.title.id)
     post_url = request.POST['post-url']
     title_obj.link = post_url
     title_obj.save()
     
     post_obj.link = post_url
-    post_obj.title = title_obj
     post_obj.save()
 
 def update_post_tabs(request, post_obj, lang_obj_list):
@@ -168,27 +172,27 @@ def update_post_print_opts(request, lang_obj_list):
     update_dictionaryitem(request, lang_obj_list, '-print', PRINT_ID)
     update_dictionaryitem(request, lang_obj_list, '-print-img', PRINT_IMG_ID)
 
-def update_post_textblocks(request, post_obj, lang_obj, text_type, base_ind):
-    tab_obj = post_obj.tab_set.get(language=lang_obj)
+def update_post_textblocks(request, post_obj, lang_obj,
+        tab_obj, text_type):
     
     # Create regex and filter for TextBlock data in request.POST
     regex_base_pattern = lang_obj.full_name + '-' + text_type + '-'
-    regex = re.compile('^' + regex_base_pattern + '(?P<item_ind>\d+)$')
-    item_dict = sub_dict(request.POST, regex)
+    regex = re.compile('^' + regex_base_pattern + '(?P<tab_ind>\d+)$')
+    block_dict = sub_dict(request.POST, regex)
     
-    block_count = 0
     for block in block_dict.keys():
+        # Retrieve actual block text and skip if empty
+        block_text = block_dict[block]
+        if not block_text:
+            continue
+        
         # Determine position of text block in tab (tab_index)
-        match = regex.search(block)
-        block_ind = match.group('block_ind')
-        tab_ind = block_ind + base_ind
-        block_count += 1
-        
-        # Retrieve actual block text 
-        block_text = block_dict[key]
-        
+        match = regex.match(block)
+        tab_ind_str = match.group('tab_ind')
+        tab_ind = int(tab_ind_str)
+
         # Determine if textblock should be included for printing option
-        print_field = regex_base_pattern + 'print-' + block_ind
+        print_field = regex_base_pattern + 'print-' + tab_ind_str
         block_print = request.POST.get(print_field, False)
         
         # Update existing TextBlock or create new
@@ -200,13 +204,44 @@ def update_post_textblocks(request, post_obj, lang_obj, text_type, base_ind):
                 tab=tab_obj,
                 text=block_text,
                 tab_index=tab_ind,
-                printable=block_print
-                post_intro=(text_type == intro),
+                printable=block_print,
+                post_intro=(text_type == 'intro'),
             )
         else:
             block_obj.text = block_text
             block_obj.printable = block_print
-    return base_ind + block_count
+
+def update_post_images(request, post_obj, lang_obj_list, main_img_bool):
+    regex = re.compile('^img-(?P<tab_ind>\d+)$')
+    img_dict = sub_dict(request.POST, regex)
+    for img in img_dict.keys():
+        match = regex.match(img)
+        tab_ind_str = match.group('tab_ind')
+        tab_ind = int(tab_ind_str)
+        img_base = 'img-' + tab_ind_str + '-'
+        img_link = request.POST[img_base + 'link']
+        img_width = request.POST[img_base + 'width']
+        img_height = request.POST[img_base + 'height']
+        wp_img_id = request.POST.get(img_base + 'wpid', False)
+        
+        try:
+            img_obj = post_obj.image_set.get(tab_index=tab_ind)
+        except Image.DoesNotExist:
+            img_obj = post_obj.image_set.create(
+                tab_index=tab_ind,
+                post_main=main_img_bool,
+                link=img_link,
+                width=img_width,
+                height=img_height,
+            )
+        if wp_img_id:
+            img_obj.wordpress_img_id=wp_img_id
+        
+        for lang_obj in lang_obj_list:
+            caption_key = lang_obj.full_name + '_caption'
+            caption_field = base_pattern + '-' + lang_obj.full_name
+            caption_text = request.POST[caption_field]
+            img_obj.__dict__[caption_key]=caption_text
 
 def UpdatePost(request, post_id):
     post_obj = get_object_or_404(Post, pk=post_id)
@@ -216,14 +251,14 @@ def UpdatePost(request, post_id):
     lang_dict = update_post_tabs(request, post_obj, lang_obj_list)
     update_post_print_opts(request, lang_obj_list)
     
-    base_tab_ind = 0
-    
     for lang_obj in lang_obj_list:
         if not lang_dict[lang_obj.full_name]:
             continue
-        base_tab_ind = update_post_textblocks(
-                        request, post_obj, lang_obj, 'intro', base_tab_ind)
+        tab_obj = post_obj.tab_set.get(language=lang_obj)
+        update_post_textblocks(request, post_obj, lang_obj,
+            tab_obj, 'intro')
     
+    update_post_images(request, post_obj, lang_obj_list, True)
     return HttpResponseRedirect(reverse('post_generator:post',
                                         args=(post_obj.id,)))
 
