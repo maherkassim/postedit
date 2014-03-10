2ca10cf4cfda1fd3440777ab163f740fa31b9d2c
Maher Kassim <kassim.maher@gmail.com>
2014-03-09 21:07:20 -0400

    Updated Views __init__ to accomodate removal of old Views

--- post_generator/views/__init__.py
+++ post_generator/views/__init__.py
@@ -1,7 +1,2 @@
-from details import IndexView
-from details import PostDetailView
-from editpost import NewPost
-from editpost import EditPost
-from editpost import UpdatePost
 from post import ManagePost
 from post import ViewPost
